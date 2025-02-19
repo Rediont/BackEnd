10eb874fc201b5272ce762c8bb48466287a2917f
Rediont <mishunyaka14@gmail.com>
2025-02-19 20:07:34 +0200

    created basic login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const User = require('../models/user.model')
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send('login')
+router.get('/', async (req,res) => {
+    try {
+        const user = await User.find({username: req.body.username, password: req.body.password}).lean();
+        if(user){
+            res.json({
+                id: user.employyeId,
+                privileges: user.privileges
+            });
+        }
+        else {
+            res.json({
+                message: 'Acces denied!'
+            });
+        }
+    }
+    catch {
+        res.status(500);
+    }
 })
 
 module.exports = router;
